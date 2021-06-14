ca8cfe613b4ae409c1545d1a954c7a1a5efde970
Крайнюков Сергей <skrajnjukov@rctahograf.ru>
2021-06-14 15:02:04 +0300

    custom tool bar created

--- FinancialApp/app/src/main/java/com/darklight_systems/financialapp/MainActivity.kt
+++ FinancialApp/app/src/main/java/com/darklight_systems/financialapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.darklight_systems.financialapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.tool_bar_with_menu_button.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var leftMenuTitlesArray:Array<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createToolbar()
+    }
+
+    private fun createToolbar() {
+        setSupportActionBar(tool_bar)
     }
 }
